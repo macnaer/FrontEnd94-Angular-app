e51942fda5a9dd3976a221fd029b977b4b5ab274
k2 <trofimchuk.an@gmail.com>
2020-12-01 20:25:55 +0200

    Styles  for event list

--- src/app/event/event-list/event-list.component.ts
+++ src/app/event/event-list/event-list.component.ts
@@ -9,7 +9,8 @@ import { Event } from '../event.model';
 export class EventListComponent implements OnInit {
   events: Event[] = [
     new Event('Holly JS', 'This is js event', 'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg'),
-    new Event('Dot Net Fest', 'This .NET', 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Microsoft_.NET_logo.png')
+    new Event('Dot Net Fest', 'This .NET', 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Microsoft_.NET_logo.png'),
+    new Event('DevOps conf', 'This DevOps conf best for..', 'https://s27389.pcdn.co/wp-content/uploads/2020/04/automating-six-cs-devops-1013x440.jpg')
   ];
 
   constructor() { }
